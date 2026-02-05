2e42caa534f7ea21c416a0b145d2f1a28b63999e
a24julianlt <a24julianlt@iessanclemente.net>
2026-02-05 09:44:11 +0100

    guardar partidas

--- AD-UD4-AT.03-MongoDB_Videoxogos/demo/src/main/java/com/controller/PartidaController.java
+++ AD-UD4-AT.03-MongoDB_Videoxogos/demo/src/main/java/com/controller/PartidaController.java
@@ -1,14 +1,38 @@
 package com.controller;
 
-import org.bson.Document;
+import org.bson.*;
 
 import com.model.Partida;
 import com.mongodb.client.MongoCollection;
 
 public class PartidaController {
-    private final MongoCollection<Document> collection = new MongoProvider().partida();
+    private final MongoProvider mongo = new MongoProvider();
 
     public Partida crearPartida(String xogador, String xogo, int puntuacion, float duracion, int nivel) {
         return new Partida(xogador, xogo, puntuacion, duracion, nivel);
+    }
+
+    public void guardarPartida(Partida par) {
+        try {
+            MongoCollection<Document> collection = mongo.partida();
+
+            Document docPartida = new Document("xogador", par.getXogador())
+                    .append("xogo", par.getXogo())
+                    .append("puntuacion", par.getPuntuacion())
+                    .append("duracion", par.getDuracion())
+                    .append("nivel", par.getNivel());
+
+            collection.insertOne(docPartida);
+        } catch (Exception e) {
+            System.out.println("ERROR AL GUARDAR UNA PARTIDA: " + e.getMessage());
+        }
+    }
+
+    public void consulta() {
+
+    }
+
+    public void close() {
+        mongo.close();
     }
 }
--- AD-UD4-AT.03-MongoDB_Videoxogos/demo/src/main/java/com/view/App.java
+++ AD-UD4-AT.03-MongoDB_Videoxogos/demo/src/main/java/com/view/App.java
@@ -1,18 +1,35 @@
 package com.view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.controller.PartidaController;
 import com.model.Partida;
 
 public class App {
     public static void main(String[] args) {
         PartidaController controller = new PartidaController();
+        List<Partida> partidas = new ArrayList<>();
 
         Partida p1 = controller.crearPartida("Mario", "Space Invaders", 1200, 15, 3);
+        partidas.add(p1);
         Partida p2 = controller.crearPartida("Luigi", "Pac-Man", 800, 10, 2);
+        partidas.add(p2);
         Partida p3 = controller.crearPartida("Mario", "Donkey Kong", 1500, 18, 4);
+        partidas.add(p3);
         Partida p4 = controller.crearPartida("Peach", "Tetris", 600, 12, 1);
+        partidas.add(p4);
         Partida p5 = controller.crearPartida("Mario", "Super Mario Bros", 2000, 25, 5);
+        partidas.add(p5);
         Partida p6 = controller.crearPartida("Luigi", "Galaga", 950, 14, 3);
+        partidas.add(p6);
 
+        // guardar partidas
+        partidas.forEach(p -> controller.guardarPartida(p));
+
+        
+
+
+        controller.close();
     }
 }
